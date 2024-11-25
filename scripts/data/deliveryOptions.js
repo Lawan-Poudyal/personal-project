1f8c2d91d4319c6c2dd83718ace9b3a756c8457b
Lawan-Poudyal <poudyallawan@gmail.com>
2024-11-25 23:28:49 +0545

    removed weekends for delivery

--- scripts/data/deliveryOptions.js
+++ scripts/data/deliveryOptions.js
@@ -1,4 +1,5 @@
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+
 export function getDeliveryOption(deliveryOptionId) {
   let deliveryOption;
 
@@ -11,12 +12,28 @@ export function getDeliveryOption(deliveryOptionId) {
   return deliveryOption || deliveryOptions[0];
 }
 
-export function calculateDeliveryDate(deliveryOption) {
-  const today = dayjs();
-  const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
-  const dateString = deliveryDate.format("dddd, MMMM D");
+function isWeekend(deliveryDate) {
+  const dayOfWeek = deliveryDate.format("dddd");
 
-  return dateString;
+  return dayOfWeek === "Satruday" || dayOfWeek === "Sunday";
+}
+
+export function calculateDeliveryDate(deliveryOption) {
+  // const today = dayjs();
+  //   const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+  //   const dateString = deliveryDate.format("dddd, MMMM D");
+  let remainingDays = deliveryOption.deliveryDays;
+  let deliveryDate = dayjs();
+
+  while (remainingDays > 0) {
+    deliveryDate = deliveryDate.add(1, "days");
+
+    if (!isWeekend(deliveryDate)) {
+      remainingDays--;
+    }
+  }
+
+  return deliveryDate.format("dddd, MMMM D");
 }
 
 export const deliveryOptions = [
