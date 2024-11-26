35b6ff9728e02371e83a9aff92144baf4f5b09d6
Lawan-Poudyal <poudyallawan@gmail.com>
2024-11-26 22:59:01 +0545

    testing money.js

--- tests-jasmine/moneyTest.js
+++ tests-jasmine/moneyTest.js
@@ -8,4 +8,7 @@ describe("test Suite: format money", () => {
   it("works with 0", () => {
     expect(formatMoney(0)).toEqual("0.00");
   });
+  it("rounds to nthe earest cent", () => {
+    expect(formatMoney(2000.5)).toEqual("20.01");
+  });
 });
